1cbd38319c9f5cb5f773865c8bb2f41c2f741599
ikisis <ikisis@gmail.com>
2023-01-12 15:19:41 +0900

    add erc3156 flashloan for stakable token

--- eth/contracts/StakableToken.sol
+++ eth/contracts/StakableToken.sol
@@ -3,6 +3,8 @@ pragma solidity >=0.8.0;
 
 import "./MintableToken.sol";
 import "./libs/SafeTransfer.sol";
+import "./interfaces/IERC3156FlashBorrower.sol";
+
 
 contract StakableToken is MintableToken {
     using SafeMath for uint;
@@ -12,19 +14,36 @@ contract StakableToken is MintableToken {
 
     uint public reserved;
 
+    uint public flashFee = 3; // 1 == 0.01%
+
     constructor(address _token)
     MintableToken(
-        string(abi.encodePacked("Nigo Staked", StakableToken(token).name())),
-        string(abi.encodePacked("ng", StakableToken(token).symbol()))) {
+        string(abi.encodePacked("Nigo Staked ", StakableToken(token).name())),
+        string(abi.encodePacked("st", StakableToken(token).symbol()))) {
         token = _token;
     }
 
-    function stake(address from) external returns(uint staked){
+    uint8 locked = 0;
 
-        uint balance = IERC20(token).balanceOf(address(this));
+    bool private unlocked = true;
+
+    modifier lock() {
+        require(unlocked == true, 'Nigo: LOCKED');
+        unlocked = false;
+        _;
+        unlocked = true;
+    }
+
+    function _stakedBalance() private view returns(uint) {
+        return IERC20(token).balanceOf(address(this));
+    }
+
+    function stake(address from) external returns(uint staked){
+        // before staking, staker should transfer token to this contract
+        uint balance = _stakedBalance();
         uint amount = balance.sub(reserved);
 
-        require(amount > 0, "not found staked value");
+        require(amount > 0, "nigo: not found staked value");
 
         if(totalSupply == 0) {
             staked = amount;
@@ -39,15 +58,42 @@ contract StakableToken is MintableToken {
     }
 
     function unstake(address from, uint staked) external returns(uint amount) {
-
-        amount = IERC20(token).balanceOf(address(this)).mul(staked) / totalSupply;
+        
+        uint balance = _stakedBalance();
+        amount = balance.mul(staked) / totalSupply;
 
         _burn(from, staked);
 
         token._transfer( from, amount);
 
-        reserved = IERC20(token).balanceOf(address(this));
+        reserved = balance;
 
     }
+
+    function erc3156(
+        IERC3156FlashBorrower receiver,
+        uint amount,
+        bytes calldata data
+    ) external returns(bool) {
+
+        token._transfer(address(receiver), amount);
+
+        require(
+            receiver.onFlashLoan(address(receiver), token, amount, flashFee, data)
+            == keccak256("ERC3156FlashBorrower.onFlashLoan"),
+            "nigo: IERC3156 callback failed");
+
+        uint balance = _stakedBalance();
+
+        require(
+            balance * 1000 > reserved  * 1000 + (amount * flashFee), 
+            "nigo: not enough repayment");
+
+        reserved = balance;
+
+        return true;
+        
+    }
+
 
 }
