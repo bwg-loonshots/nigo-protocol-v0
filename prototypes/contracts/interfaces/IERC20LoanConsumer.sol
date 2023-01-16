a297a2d96699a057f5935535846923f9b1654725
ikisis <ikisis@gmail.com>
2023-01-16 17:02:34 +0900

    remove prototype

--- prototypes/contracts/interfaces/IERC20LoanConsumer.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.7.0 < 0.9.0;
-
-interface IERC20LoanConsumer {
-
-    function perform(address sender, address token, uint256 amount) external;
-
-}
