cc82429c205359da270dfa765f6ecfcfd096febb
jUnYOUngLee96 <junyoung.lee@bankwareglobal.com>
2023-01-26 10:28:57 +0900

    [20230126]add loading animation

---
+++ dapp/web3js/src/main/resources/static/js/loading.js
@@ -0,0 +1,7 @@
+function showLoading(){
+	document.getElementById("loading-body").style.display = "block";
+}
+
+function hideLoading(){
+	document.getElementById("loading-body").style.display = "none";
+}
