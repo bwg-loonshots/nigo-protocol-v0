6abbf5e9ba27b4f660ff842f24beed0e269dbc44
jUnYOUngLee96 <junyoung.lee@bankwareglobal.com>
2023-01-30 13:25:32 +0900

    add test token info

--- dapp/web3js/src/main/resources/static/js/tokenDatas.js
+++ dapp/web3js/src/main/resources/static/js/tokenDatas.js
@@ -6,6 +6,30 @@ const tokenDatas = [
 		image : "../img/ethereum.webp"
 	},
 	{
+		name:"Charsiu",
+		symbol:"CHS",
+		address:"0xE4F088C428bF8D698c429db056FF17238e7B5469",
+		image : "../img/charsiu.jfif"
+	},
+	{
+		name:"Tamago",
+		symbol:"TMG",
+		address:"0xa99e1830bC3593D2dBe35A543a978c1c3dF9FB17",
+		image : "../img/tamago.jfif"
+	},
+	{
+		name:"Mineral",
+		symbol:"MNR",
+		address:"0x96d3DD0348e69f9e43a93C16da76122f955faaa2",
+		image : "../img/mineral.png"
+	},
+	{
+		name:"Vespene Gas",
+		symbol:"VSP",
+		address:"0xf592926559815fBCd8f08c207676F5F3608EE9e1",
+		image : "../img/vespene.png"
+	},
+	{
 		name:"Token01",
 		symbol:"T01",
 		address:"0xE12d5180Bd953d9a62E5f3a8f7941Ca61e5588dc",
